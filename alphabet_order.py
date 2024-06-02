2f068772a3ecdcb00261d129a6b37c2e16d2c3e1
b-poole <Barrettpoole14@gmail.com>
2024-06-02 17:21:44 -0400

    Add files via upload

--- alphabet_order.py
+++ alphabet_order.py
@@ -4,20 +4,30 @@ Created on Thu May 30 12:43:48 2024
 
 @author: barre
 """
-S = 'ikyqqcvlaepuasdawdas'
-ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
-ans = ''
-temp = ''
+def main():
+    """Program to print longest substring in alphabetical order, given a word"""
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    ans = ''
+    temp = ''
 
-for letter in S:
-    if temp == '':
-        temp = letter
-    elif(ALPHABET.index(letter) >= ALPHABET.index(temp[-1])):
-        temp += letter
-    else:
-        if len(temp) > len(ans):
-            ans = temp
-        temp = letter
-if len(temp) > len(ans):
-    ans = temp
-print("Longest substring in alphabetical order is: " + ans)
+    while True:
+        word = input("Please enter a word: ").lower()
+        if len(word) > 0:
+            break
+        print("Invalid input, please provide a word.")
+
+    for letter in word:
+        if temp == '':
+            temp = letter
+        elif(alphabet.index(letter) >= alphabet.index(temp[-1])):
+            temp += letter
+        else:
+            if len(temp) > len(ans):
+                ans = temp
+            temp = letter
+    if len(temp) > len(ans):
+        ans = temp
+    print("Longest substring in alphabetical order is: " + ans)
+
+if __name__ == '__main__':
+    main()
